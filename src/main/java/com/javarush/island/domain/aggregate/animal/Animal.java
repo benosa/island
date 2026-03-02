13acae90a1f248e4f2714899b694f170651339a9
benosa <id@bevolved.net>
2026-03-02 11:52:00 +0300

    абстрактный класс Animal с базовыми полями и методами

---
+++ src/main/java/com/javarush/island/domain/aggregate/animal/Animal.java
@@ -0,0 +1,116 @@
+package com.javarush.island.domain.aggregate.animal;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public abstract class Animal implements Organism {
+
+    protected double weight;
+    protected int maxPerCell;
+    protected int speed;
+    protected double foodNeeded;
+    protected double currentFood;
+    protected boolean alive = true;
+    protected boolean reproduced = false;
+    protected volatile boolean processed = false;
+
+    protected int row;
+    protected int col;
+
+    public Animal(double weight, int maxPerCell, int speed, double foodNeeded) {
+        this.weight = weight;
+        this.maxPerCell = maxPerCell;
+        this.speed = speed;
+        this.foodNeeded = foodNeeded;
+        this.currentFood = foodNeeded;
+    }
+
+    public abstract Animal reproduce();
+
+    public abstract boolean eat(Organism prey);
+
+    public Direction chooseDirection() {
+        Direction[] directions = Direction.values();
+        return directions[ThreadLocalRandom.current().nextInt(directions.length)];
+    }
+
+    public void consumeEnergy() {
+        if (foodNeeded == 0) return;
+        currentFood -= foodNeeded * 0.1;
+        if (currentFood < 0) {
+            alive = false;
+        }
+    }
+
+    public boolean isHungry() {
+        return currentFood < foodNeeded;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void die() {
+        alive = false;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getMaxPerCell() {
+        return maxPerCell;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public double getFoodNeeded() {
+        return foodNeeded;
+    }
+
+    public double getCurrentFood() {
+        return currentFood;
+    }
+
+    public void setCurrentFood(double currentFood) {
+        this.currentFood = currentFood;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public boolean isReproduced() {
+        return reproduced;
+    }
+
+    public void setReproduced(boolean reproduced) {
+        this.reproduced = reproduced;
+    }
+
+    public boolean isProcessed() {
+        return processed;
+    }
+
+    public void setProcessed(boolean processed) {
+        this.processed = processed;
+    }
+
+    public void resetState() {
+        this.reproduced = false;
+        this.processed = false;
+    }
+}
