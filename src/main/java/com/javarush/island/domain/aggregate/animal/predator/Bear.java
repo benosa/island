78dda96e9a9650f72d9ad061e7823ad5d9a7ebe6
benosa <id@bevolved.net>
2026-03-03 12:07:00 +0300

    добавил Лису, Медведя и Орла

---
+++ src/main/java/com/javarush/island/domain/aggregate/animal/predator/Bear.java
@@ -0,0 +1,23 @@
+package com.javarush.island.domain.aggregate.animal.predator;
+
+public class Bear extends Predator {
+
+    public Bear() {
+        super(500, 5, 2, 80);
+    }
+
+    @Override
+    public Bear reproduce() {
+        return new Bear();
+    }
+
+    @Override
+    public String getIcon() {
+        return "\uD83D\uDC3B";
+    }
+
+    @Override
+    public String getName() {
+        return "Bear";
+    }
+}
---
+++ src/main/java/com/javarush/island/domain/aggregate/animal/predator/Eagle.java
@@ -0,0 +1,23 @@
+package com.javarush.island.domain.aggregate.animal.predator;
+
+public class Eagle extends Predator {
+
+    public Eagle() {
+        super(6, 20, 3, 1);
+    }
+
+    @Override
+    public Eagle reproduce() {
+        return new Eagle();
+    }
+
+    @Override
+    public String getIcon() {
+        return "\uD83E\uDD85";
+    }
+
+    @Override
+    public String getName() {
+        return "Eagle";
+    }
+}
