cb23242659c8c2ccfbb66c8ce8c2ae54265f810f
benosa <id@bevolved.net>
2026-03-04 10:08:00 +0300

    травоядные: Мышь и Коза

---
+++ src/main/java/com/javarush/island/domain/aggregate/animal/herbivore/Goat.java
@@ -0,0 +1,23 @@
+package com.javarush.island.domain.aggregate.animal.herbivore;
+
+public class Goat extends Herbivore {
+
+    public Goat() {
+        super(60, 140, 3, 10);
+    }
+
+    @Override
+    public Goat reproduce() {
+        return new Goat();
+    }
+
+    @Override
+    public String getIcon() {
+        return "\uD83D\uDC10";
+    }
+
+    @Override
+    public String getName() {
+        return "Goat";
+    }
+}
