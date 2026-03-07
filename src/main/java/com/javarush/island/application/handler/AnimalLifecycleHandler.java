8acc79b2984bcb2449826b33b2245ab48a326c6c
benosa <id@bevolved.net>
2026-03-07 11:11:00 +0300

    хэндлер жизненного цикла животных

---
+++ src/main/java/com/javarush/island/application/handler/AnimalLifecycleHandler.java
@@ -0,0 +1,21 @@
+package com.javarush.island.application.handler;
+
+import com.javarush.island.domain.ports.in.SimulationUseCase;
+
+public class AnimalLifecycleHandler implements Runnable {
+
+    private final SimulationUseCase simulationUseCase;
+
+    public AnimalLifecycleHandler(SimulationUseCase simulationUseCase) {
+        this.simulationUseCase = simulationUseCase;
+    }
+
+    @Override
+    public void run() {
+        try {
+            simulationUseCase.processLifecycleTick();
+        } catch (Exception e) {
+            System.err.println("Ошибка в жизненном цикле: " + e.getMessage());
+        }
+    }
+}
