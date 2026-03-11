5acaf67620f75548209b1dc2b0fbe9b7c5276737
benosa <id@bevolved.net>
2026-03-11 10:24:00 +0300

    переделал вывод статистики, каждый вид отдельной строкой

--- src/main/java/com/javarush/island/infrastructure/adapters/ConsoleStatisticsAdapter.java
+++ src/main/java/com/javarush/island/infrastructure/adapters/ConsoleStatisticsAdapter.java
@@ -24,52 +24,47 @@ public class ConsoleStatisticsAdapter implements StatisticsPort {
             Map.entry("Caterpillar", "\uD83D\uDC1B")
     );
 
+    private static final String[] PREDATOR_NAMES = {"Wolf", "Snake", "Fox", "Bear", "Eagle"};
+    private static final String[] HERBIVORE_NAMES = {"Horse", "Deer", "Rabbit", "Mouse", "Goat",
+            "Sheep", "Boar", "Buffalo", "Duck", "Caterpillar"};
+
     @Override
     public void displayStatistics(int tick, Map<String, Integer> animalCounts, int totalPlants, int totalAnimals) {
         clearConsole();
 
-        System.out.println("\u2554" + "\u2550".repeat(58) + "\u2557");
-        System.out.printf("\u2551 Такт: %-5d | Животных: %-8d | Растений: %-6d \u2551%n",
-                tick, totalAnimals, totalPlants);
-        System.out.println("\u2560" + "\u2550".repeat(58) + "\u2563");
+        int width = 40;
+        System.out.println("\u2554" + "\u2550".repeat(width) + "\u2557");
+        printLine(String.format(" Такт: %d | Животных: %d | Растений: %d", tick, totalAnimals, totalPlants), width);
+        System.out.println("\u2560" + "\u2550".repeat(width) + "\u2563");
 
-        StringBuilder predators = new StringBuilder("\u2551 Хищники:    ");
-        StringBuilder herbivores = new StringBuilder("\u2551 Травоядные:  ");
-
-        String[] predatorNames = {"Wolf", "Snake", "Fox", "Bear", "Eagle"};
-        String[] herbivoreNames = {"Horse", "Deer", "Rabbit", "Mouse", "Goat",
-                "Sheep", "Boar", "Buffalo", "Duck", "Caterpillar"};
-
-        for (String name : predatorNames) {
+        printLine(" \u2501\u2501 Хищники:", width);
+        for (String name : PREDATOR_NAMES) {
             int count = animalCounts.getOrDefault(name, 0);
-            predators.append(String.format("%s%d ", ICONS.get(name), count));
+            printLine(String.format("   %s %-12s %d", ICONS.get(name), name, count), width);
         }
-        padLine(predators, 59);
-        System.out.println(predators + "\u2551");
 
-        for (String name : herbivoreNames) {
+        printLine(" \u2501\u2501 Травоядные:", width);
+        for (String name : HERBIVORE_NAMES) {
             int count = animalCounts.getOrDefault(name, 0);
-            herbivores.append(String.format("%s%d ", ICONS.get(name), count));
+            printLine(String.format("   %s %-12s %d", ICONS.get(name), name, count), width);
         }
-        padLine(herbivores, 59);
-        System.out.println(herbivores + "\u2551");
 
-        String plantLine = String.format("\u2551 Растения:    \uD83C\uDF3F%d", totalPlants);
-        StringBuilder plantSb = new StringBuilder(plantLine);
-        padLine(plantSb, 59);
-        System.out.println(plantSb + "\u2551");
+        printLine(String.format(" \uD83C\uDF3F Растения:     %d", totalPlants), width);
 
-        System.out.println("\u255A" + "\u2550".repeat(58) + "\u255D");
+        System.out.println("\u255A" + "\u2550".repeat(width) + "\u255D");
+    }
+
+    private void printLine(String content, int width) {
+        StringBuilder sb = new StringBuilder("\u2551" + content);
+        while (sb.length() < width + 1) {
+            sb.append(' ');
+        }
+        sb.append("\u2551");
+        System.out.println(sb);
     }
 
     private void clearConsole() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
-    }
-
-    private void padLine(StringBuilder sb, int targetWidth) {
-        while (sb.length() < targetWidth) {
-            sb.append(' ');
-        }
     }
 }
